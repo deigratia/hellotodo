fefd079bdb7e54ef3e37991b1c6e1cead9302c90
Dei Gratia R R <rdeigratia95@gmail.com>
2018-04-06 15:31:16 +0700

    update hellotodo

---
+++ Javascript/index.js
@@ -0,0 +1,57 @@
+const inputText = document.getElementById('inputText');
+const outputText = document.getElementById('outputText');
+const addButton = document.getElementById('addButton');
+
+var label = []
+
+// fungsi untuk tambah list ke dom
+const showList = function(){
+  outputText.innerHTML = "";
+  label.forEach((data,index) => {
+    var create = document.createElement('div')
+    create.id = index;
+    create.innerHTML =`<span>${data}</span>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+    <b><i onclick="deleteTodo(${index})">Delete</i><b>&nbsp|
+    <b><i onclick="editTodo(${index})">Edit</i><b>`;
+
+    outputText.appendChild(create)
+  })
+}
+
+//cek if localStorage.data
+if (localStorage.data) {
+  label = JSON.parse(localStorage.data)
+  showList()
+}
+
+//fungsi nambah
+const addTodo = function () {
+  label.push(inputText.value)
+  localStorage.data = JSON.stringify(label)
+  showList()
+}
+
+//fungsi hapus semua
+const clearTodo = function() {
+  label = [];
+  outputText.innerHTML = "";
+  localStorage.clear()
+}
+
+//fungsi deleteTodo
+const deleteTodo = function(index){
+  label.splice(index, 1)
+  localStorage.data = JSON.stringify(label);
+  showList()
+}
+
+//fungsi edit
+const editTodo =function(index) {
+  var edit = prompt('Edit Text')
+  label[index] = edit
+  localStorage.data = JSON.stringify(label);
+  showList()
+}
+
+addButton.addEventListener("click", addTodo)
+addButton1.addEventListener("click", clearTodo)
